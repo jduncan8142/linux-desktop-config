fb33e4e47604d061406fb2ca7bbb752a3f06c461
Jason <jason.matthew.duncan@gmail.com>
2021-10-23 22:47:00 -0500

    Added i3, alacritty, and fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
